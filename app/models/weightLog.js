e51c416034d12681643465ab7306115ea0dede17
WaqasMumtaz <waqasali.mumtaz95@gmail.com>
2020-05-19 23:00:29 +0500

    add arms schema

--- app/models/weightLog.js
+++ app/models/weightLog.js
@@ -8,6 +8,7 @@ const weightSchema = new Schema({
     time:{type:String},
     month:{type:Number},
     weight:{type:String},
+    arms:{type:arms},
     day:{type:String},
     dayOfMonth:{type:Number},
     biceps:{type:String},
